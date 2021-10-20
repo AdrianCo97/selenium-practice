64596fb529113f15cac4d6b2aff2dca4ed5d70d0
AdrianCo97 <adrian.corazza@outlook.com>
2021-10-20 15:51:42 +0200

    Added a bunch of html selectors

--- src/test/java/HelloSeleniumTest.java
+++ src/test/java/HelloSeleniumTest.java
@@ -1,7 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelloSeleniumTest {
@@ -16,6 +18,25 @@ public class HelloSeleniumTest {
     public void firstTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
+
+        WebElement element;
+
+        element = driver.findElement(By.id("user-name"));
+
+        //driver.findElement(By.name("name of locator"));
+
+        //finds the element with the classname form_input
+        driver.findElement(By.className("form_input"));
+
+        //finds the element with the tagName input
+        driver.findElement(By.tagName("input"));
+
+        //finds the element with the cssSelector #user-name
+        driver.findElement(By.cssSelector("#user-name"));
+
+        //finds the element with the xpath //*[@id="user-name"]
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
+
         driver.quit();
     }
 }
